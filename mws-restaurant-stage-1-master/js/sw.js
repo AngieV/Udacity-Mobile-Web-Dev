60c83364e5e97457aec7bb13198745ea15035b60
AngieV <angiev@me.com>
2018-08-20 18:16:50 -0500

    add serviceworker and cache

--- mws-restaurant-stage-1-master/js/sw.js
+++ mws-restaurant-stage-1-master/js/sw.js
@@ -4,33 +4,19 @@ var allCaches = [
   staticCacheName,
   contentImgsCache
 ];
+var urlsToCache = ['/skeleton',
+                   '/js/main.js',
+                   '/js/restaurant_info.js',
+                   '/css/styles.css',
+                  ]
 
 self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/skeleton',
-        'js/main.js',
-        'js.restaurant_info.js'
-        'css/styles.css',
-        'img/',
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('restaurant_reviews-') &&
-                 !allCaches.includes(cacheName);
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -47,13 +33,41 @@ self.addEventListener('fetch', function(event) {
       return;
     }
   }
-
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(staticCacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 
 function servePhoto(request) {
   var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
@@ -69,9 +83,3 @@ function servePhoto(request) {
     });
   });
 }
-
-self.addEventListener('review', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
